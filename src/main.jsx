272bc7e7eb2d8b80b00810e0b6b5374adeeca617
Sanjay8602 <sanjuhsuthar@gmail.com>
2026-02-06 13:04:46 +0530

    Add .nojekyll

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+//import { BrowserRouter } from 'react-router-dom'
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const basename =
+  window.location.hostname === "reviewmycafe.in" ? "/" : "/Review-cafe/";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
-)
+);
